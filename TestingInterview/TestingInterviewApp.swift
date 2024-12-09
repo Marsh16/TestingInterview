82f1b970ffc234d4e5cb99e06638b4b1b9c4246f
Marsh16 <94427962+Marsh16@users.noreply.github.com>
2024-12-09 09:30:36 +0700

    Initial Commit

---
+++ TestingInterview/TestingInterviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingInterviewApp.swift
+//  TestingInterview
+//
+//  Created by Marsha Likorawung on 09/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TestingInterviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
